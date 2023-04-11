1cfe9c46f355b1352611a0adb24fd25c9e4efead
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-04-12 00:29:16 +0700

    created Library.java and added Library.MainMenu

---
+++ Library.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Library {
+    public static void MainMenu() {
+        Scanner sc = new Scanner(System.in);
+        String user_input = "";
+        System.out.println("""
+                    Подобрать Laptop :
+                    1.по ОЗУ
+                    2.по ПЗУ
+                    3.по ОС
+                    4.по цвету
+                    5.Выход""");
+
+
+        while (!(user_input.equals("5"))) {
+            user_input=sc.nextLine();
+
+        }
+    }
+}
